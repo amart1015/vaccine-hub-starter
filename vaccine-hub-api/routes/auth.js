c5bd503dd9b191447d2c21d6c23165f620f17c2a
amart1015 <amart1015@fiu.edu>
2022-06-28 14:19:09 -0400

    Connected API to postgres.

---
+++ vaccine-hub-api/routes/auth.js
@@ -0,0 +1,22 @@
+const express=require("express")
+const User = require("../models/user");
+const router=express.Router()
+
+router.post("/login", async(req,res,next)=>{
+    try{
+
+    }catch(err){
+        next(err)
+    }
+})
+
+router.post("/register", async(req,res,next)=>{
+    try{
+
+    }catch(err){
+        next(err)
+    }
+})
+
+module.exports=router
+
